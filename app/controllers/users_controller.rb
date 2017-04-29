37de3a5fa7ec7ca58368336cef113dd2978546f5
Karan Oberoi <koberoi1017@gmail.com>
2017-04-29 16:26:36 -0500

    users show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,10 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.search(params[:search])
+  end
+
+  def show
+    @users = User.all
   end
 
 end
