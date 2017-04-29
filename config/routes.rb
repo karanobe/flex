bf46ca4c8d2b7260f43e99a1932c09ed9077f34e
Elsie Koglin <email": null>
2017-04-29 18:10:13 -0500

    Changed nav bar path

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   resource  :preferences
   devise_for :users, :controllers => { registrations: 'registrations' }
   resource  :users
+  resources :gyms
   root 'welcome#index'
 
-  resources :gyms
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
