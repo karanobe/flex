672ac9de1e8384891a14b4ee1281856714567789
Elsie Koglin <email": null>
2017-04-29 16:49:12 -0500

    Stil fixing Ted's things

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,10 +79,10 @@ Rails.application.configure do
 config.paperclip_defaults = {
   storage: :s3,
   s3_credentials: {
-    bucket: ENV.fetch('flex-pics'),
+    bucket: ENV.fetch('S3_BUCKET'),
     access_key_id: ENV.fetch('AWS_ACCESS_KEY'),
     secret_access_key: ENV.fetch('SECRET_ACCESS_KEY'),
-    s3_region: ENV.fetch('us-west-2'),
+    s3_region: ENV.fetch('S3_REGION'),
   }
 }
 
