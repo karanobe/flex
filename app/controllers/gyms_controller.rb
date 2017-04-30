4d6929f9d58d422bc5b3527c1f82754f9f2ed6a5
Karan Oberoi <koberoi1017@gmail.com>
2017-04-29 21:44:18 -0500

    All gyms for current user JSON

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -1,7 +1,11 @@
 class GymsController < ApplicationController
   def index
-    google = ENV['GOOGLE']
+    gyms = current_user.gyms
+    render json: gyms
+  end
 
+  def new
+    google = ENV['GOOGLE']
     render :index, locals: {google_key: google}
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+# Users
+ko = User.create!(first_name: "Karan", last_name: "Oberoi", age: 25, gender_pronoun: "He/Him", profile_bio: "Profile Biography", email: "k@gmail.com", password: "password", password_confirmation: "password")
+tn = User.create!(first_name: "Ted", last_name: "N", age: 23, gender_pronoun: "He/Him", profile_bio: "Profile Biography", email: "t@gmail.com", password: "password", password_confirmation: "password")
+sg = User.create!(first_name: "Sara", last_name: "Gerou", age: 25, gender_pronoun: "She/Her", profile_bio: "Profile Biography", email: "s@gmail.com", password: "password", password_confirmation: "password")
+ek = User.create!(first_name: "Elsie", last_name: "Koglin", age: 25, gender_pronoun: "She/Her", profile_bio: "Profile Biography", email: "e@gmail.com", password: "password", password_confirmation: "password")
+dg = User.create!(first_name: "Derek", last_name: "Gregorivich", age: 25, gender_pronoun: "He/Him", profile_bio: "Profile Biography", email: "d@gmail.com", password: "password", password_confirmation: "password")
+cf = User.create!(first_name: "Chris", last_name: "Frazz", age: 25, gender_pronoun: "He/Him", profile_bio: "Profile Biography", email: "c@gmail.com", password: "password", password_confirmation: "password")
+bw = User.create!(first_name: "Brendan", last_name: "White", age: 25, gender_pronoun: "He/Him", profile_bio: "Profile Biography", email: "b@gmail.com", password: "password", password_confirmation: "password")
+ao = User.create!(first_name: "Alex", last_name: "Origitano", age: 25, gender_pronoun: "She/Her", profile_bio: "Profile Biography", email: "a@gmail.com", password: "password", password_confirmation: "password")
+
+
+# Gyms
+Gym.create!(name: "24 Hour Fitness", street_address: "111 Street Ave.", city: "Corona", state: "CA", zip: 92879)
+Gym.create!(name: "LA Fitness", street_address: "222 Street Ave.", city: "Corona", state: "CA", zip: 92879)
+Gym.create!(name: "Fitness 19", street_address: "333 Street Ave.", city: "Corona", state: "CA", zip: 92879)
+Gym.create!(name: "Planet Fitness", street_address: "444 Street Ave.", city: "Corona", state: "CA", zip: 92879)
+Gym.create!(name: "GloboGym", street_address: "555 Street Ave.", city: "Corona", state: "CA", zip: 92879)
+Gym.create!(name: "Gold's Gym", street_address: "666 Street Ave.", city: "Corona", state: "CA", zip: 92879)
+
+
+# Memberships
+ko.memberships.create(gym_id: 1)
+ko.memberships.create(gym_id: 2)
+tn.memberships.create(gym_id: 2)
+tn.memberships.create(gym_id: 3)
+sg.memberships.create(gym_id: 3)
+sg.memberships.create(gym_id: 4)
+ek.memberships.create(gym_id: 4)
+ek.memberships.create(gym_id: 5)
+dg.memberships.create(gym_id: 5)
+dg.memberships.create(gym_id: 6)
+cf.memberships.create(gym_id: 6)
+cf.memberships.create(gym_id: 1)
+bw.memberships.create(gym_id: 1)
+bw.memberships.create(gym_id: 2)
+ao.memberships.create(gym_id: 2)
+ao.memberships.create(gym_id: 3)
+
+
+
