2a5074b9bb35696a16a8f17a178f60adfa161d46
Karan Oberoi <koberoi1017@gmail.com>
2017-05-01 12:11:34 -0500

    remove ratyrate and turbolinks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users, :controllers => { registrations: 'registrations' }, skip: [:edit, :update]
   resources  :users
   get "/users/:id/profile_load" => 'users#profile_load', :as => 'profile_load'
