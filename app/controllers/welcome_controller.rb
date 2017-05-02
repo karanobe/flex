56f473bd16b734ba3f79ed059810d5e4ffe17d3b
Jerry Gerou <saragerou@gmail.com>
2017-05-02 09:54:00 -0500

    ajaxify home

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,8 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def home
+    render json: {indexPage: render_to_string("welcome/index", :layout => false)}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   post 'friendships/:id/accept' => 'friendships#accept', as: 'accept_friend'
   delete 'friendships/:id/deny' => 'friendships#deny', as: 'deny_friend'
   root 'welcome#index'
-
+  get "/home" => "welcome#home"
   post 'twilio/voice' => 'twilio#voice'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
