cd2f63501da408da4d013d0bf19c5fe3b1657ffc
Jerry Gerou <saragerou@gmail.com>
2017-05-01 20:39:54 -0500

    reverted form to ajax

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -6,9 +6,12 @@ class GymsController < ApplicationController
 
   def new
     google_key = ENV['GOOGLE']
-    render :new, locals: {google_key: google_key}
-
+    # render :new, locals: {google_key: google}
+    render json: {newGymForm: render_to_string("gyms/new", :layout => false, locals: {google_key: google_key})}
   end
+
+
+  # need to add create route to save new gym from form submitted"
 
   def show
     gym = Gym.find(params[:id])
